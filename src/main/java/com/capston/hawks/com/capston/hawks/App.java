604226ac6935ed66852eb23da186de641074852a
Freshta Hazeq <hazeqfreshta@gmail.com>
2021-10-20 21:52:06 -0400

    This Capston Repo

---
+++ src/main/java/com/capston/hawks/com/capston/hawks/App.java
@@ -0,0 +1,13 @@
+package com.capston.hawks.com.capston.hawks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/capston/hawks/com/capston/hawks/AppTest.java
@@ -0,0 +1,20 @@
+package com.capston.hawks.com.capston.hawks;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
